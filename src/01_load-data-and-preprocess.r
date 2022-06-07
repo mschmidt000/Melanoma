a2232cb31c58ed6dcb119fb04adee1cf52fcf9e2
mschmidt000 <schmidt@izbi.uni-leipzig.de>
2022-06-07 11:30:24 +0200

    write function definitions (seurat-functions.r) and capsulated code of 01_load-data-and-preprocess.r into functions, insert section labels and TODOs

--- src/01_load-data-and-preprocess.r
+++ src/01_load-data-and-preprocess.r
@@ -1,7 +1,14 @@
 ### load data and preprocess with seurat
 ### 19.05.22
 
-pacman::p_load(Seurat, tidyverse)
+
+# load packages and source code -------------------------------------------
+
+pacman::p_load(Seurat, tidyverse, here)
+source(here("src","seurat-functions.r"))
+
+
+# define variables --------------------------------------------------------
 
 runs <- c(
   "s1_LE479_NM_T", "LE489KG_Rep", "LE493BR_Rep", "LE497NA_Rep", "LE497ST_Rep", "LE-501-DM_Rep", "LE-517-LD",
@@ -16,92 +23,18 @@ literature_path <- "literature"
 paths <- list.dirs(input_data_path, full.names = FALSE)[-which(list.dirs(input_data_path, full.names = FALSE) %in% "")]
 
 
-runs_list <- list()
-filtering_cutoff_list <- list()
+# preprocess, filter and plot ---------------------------------------------
 
-for (i in seq_along(runs)) {
-  runs_list[[runs[i]]] <- list()
-  runs_list[[runs[i]]] <- ReadMtx(
-    mtx = here(input_data_path, paths[i], "matrix.mtx.gz"), features = here(input_data_path, paths[i], "features.tsv.gz"),
-    cells = here(input_data_path, paths[i], "barcodes.tsv.gz")
-  )
-  colnames(runs_list[[runs[i]]]) <- paste(runs[i], colnames(runs_list[[runs[i]]]), sep = "_")
-  runs_list[[runs[i]]] <- CreateSeuratObject(counts = as.matrix(runs_list[[runs[i]]]), min.cells = 1, min.features = 1)
-  runs_list[[runs[i]]][["percent.mt"]] <- PercentageFeatureSet(runs_list[[runs[i]]], pattern = "^MT-")
-  runs_list[[runs[i]]][["percent.rps"]] <- PercentageFeatureSet(runs_list[[runs[i]]], pattern = "^RPS")
-  runs_list[[runs[i]]]@meta.data$orig.ident <- runs[i]
-}
+# TODO check functions for functions of packages which need to be loaded!
+# TODO test functions
+# TODO insert section labels with strg+shift+R
+runs_list <- create_seurat_object(runs = runs, paths = paths, input_data_path = input_data_path)
 
-filename <- here(figures_path, "01_quality-check.pdf")
-pdf(filename, 29.7 / 2.54, 21 / 2.54, useDingbats = FALSE)
-for (i in seq_along(runs_list)) {
-  plot(0, type = "n", axes = FALSE, xlab = "", ylab = "", xlim = c(0, 1))
-  number_cells <- ncol(runs_list[[runs[i]]]@assays$RNA@data)
-  mtext(paste0(runs[i], " (", number_cells, ")"), side = 3, line = -2, cex = 3, at = -0.04, font = 3, adj = 0)
-  print(VlnPlot(object = runs_list[[runs[i]]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt", "percent.rps")), main = runs[i])
-  print(FeatureScatter(runs_list[[runs[i]]], feature1 = "nCount_RNA", feature2 = "percent.mt"))
-  print(FeatureScatter(runs_list[[runs[i]]], feature1 = "nFeature_RNA", feature2 = "percent.mt"))
-  print(FeatureScatter(runs_list[[runs[i]]], feature1 = "percent.rps", feature2 = "percent.mt"))
-  hist(runs_list[[runs[i]]]$nCount_RNA, col = "grey80", breaks = 100)
-  hist(runs_list[[runs[i]]]$nFeature_RNA, col = "grey80", breaks = 100)
-  hist(na.omit(runs_list[[runs[i]]]$percent.mt), col = "grey80", breaks = 100)
-  hist(na.omit(runs_list[[runs[i]]]$percent.rps), col = "grey80", breaks = 100)
-}
-dev.off()
+do_qc()
 
-filename <- here(figures_path, "01_filtered-quality-check.pdf")
-pdf(filename, 29.7 / 2.54, 21 / 2.54, useDingbats = FALSE)
-for (i in seq_along(runs_list)) {
-  filtering_cutoff_list[[runs[i]]] <- c(
-    "min_nCount_RNA" = quantile(runs_list[[runs[i]]]$nCount_RNA, 0.25),
-    "max_nCount_RNA" = median(runs_list[[runs[i]]]$nCount_RNA) + sd(runs_list[[runs[i]]]$nCount_RNA) * 3,
-    "max_percent.mt" = median(runs_list[[runs[i]]]$percent.mt) + sd(runs_list[[runs[i]]]$percent.mt) * 3,
-    "max_percent.rps" = quantile(runs_list[[runs[i]]]$percent.rps, 0.30),
-    "min_nFeature_RNA" = quantile(runs_list[[runs[i]]]$nFeature_RNA, 0.25)
-  )
-
-  # filtering out dead cells
-  runs_list[[runs[i]]] <- subset(runs_list[[runs[i]]],
-    subset = nCount_RNA < filtering_cutoff_list[[i]]["min_nCount_RNA"] & percent.mt > filtering_cutoff_list[[i]]["max_percent.mt"] |
-      nFeature_RNA < filtering_cutoff_list[[i]]["min_nFeature_RNA"] & percent.mt > filtering_cutoff_list[[i]]["max_percent.mt"] |
-      percent.mt > filtering_cutoff_list[[i]]["max_percent.mt"] & percent.rps < filtering_cutoff_list[[i]]["max_percent.rps"], invert = TRUE
-  )
-
-  # filtering out doublets
-  runs_list[[runs[i]]] <- subset(runs_list[[runs[i]]],
-    subset = nCount_RNA < filtering_cutoff_list[[i]]["max_nCount_RNA"]
-  )
+runs_list <- do_filtering_and_qc(runs_list = runs_list, figures_path = figures_path)
 
 
-  plot(0, type = "n", axes = FALSE, xlab = "", ylab = "", xlim = c(0, 1))
-  number_cells <- ncol(runs_list[[runs[i]]]@assays$RNA@data)
-  mtext(paste0(runs[i], " (", number_cells, ")"), side = 3, line = -2, cex = 3, at = -0.04, font = 3, adj = 0)
-  print(VlnPlot(object = runs_list[[runs[i]]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt")), main = runs[i])
-  print(FeatureScatter(runs_list[[runs[i]]], feature1 = "nCount_RNA", feature2 = "percent.mt"))
-  print(FeatureScatter(runs_list[[runs[i]]], feature1 = "nFeature_RNA", feature2 = "percent.mt"))
-  print(FeatureScatter(runs_list[[runs[i]]], feature1 = "percent.rps", feature2 = "percent.mt"))
-  hist(runs_list[[runs[i]]]$nCount_RNA, col = "grey80", breaks = 100)
-  hist(runs_list[[runs[i]]]$nFeature_RNA, col = "grey80", breaks = 100)
-  hist(na.omit(runs_list[[runs[i]]]$percent.mt), col = "grey80", breaks = 100)
-  hist(na.omit(runs_list[[runs[i]]]$percent.rps), col = "grey80", breaks = 100)
-}
-dev.off()
+# save individual seurat objects ------------------------------------------
 
-filename <- here(figures_path, "01_top-10-variable-features.pdf")
-pdf(filename, 29.7 / 2.54, 21 / 2.54, useDingbats = FALSE)
-for (i in seq_along(runs_list)) {
-  runs_list[[runs[i]]] <- NormalizeData(runs_list[[runs[i]]]) %>%
-    FindVariableFeatures(do.plot = T, verbose = F) %>%
-    ScaleData()
-  plot1 <- VariableFeaturePlot(runs_list[[runs[i]]])
-  plot2 <- LabelPoints(plot = plot1, points = head(VariableFeatures(runs_list[[runs[i]]]), 10), repel = TRUE)
-  print(plot1 + plot2 + ggtitle(runs[i]))
-}
-dev.off()
-
-for (i in seq_along(runs_list)) {
-  seurat_obj <- runs_list[[runs[i]]]
-  filename <- here(output_data_path, paste0(runs[i], "_seurat-obj.RData"))
-  save(seurat_obj, file = filename)
-}
-rm(seurat_obj)
+save_seurat_objects(output_data_path = output_data_path, runs_list = runs_list)
---
+++ src/seurat-functions.r
@@ -0,0 +1,131 @@
+#' Create seurat object, set cell names with run-prefix and calculate mitochondrial and ribosomal expression percentage
+#'
+#' @param runs character vector of run ids
+#' @param paths character vector of individual run paths
+#' @param input_data_path character vector of general input data path
+#' @return list of seurat objects, one object per run
+create_seurat_object <- function(runs = runs, paths = paths, input_data_path = input_data_path){
+  
+  for (i in seq_along(runs)) {
+    runs_list <- list()
+    runs_list[[runs[i]]] <- list()
+    runs_list[[runs[i]]] <- Seurat::ReadMtx(
+      mtx = here(input_data_path, paths[i], "matrix.mtx.gz"), features = here(input_data_path, paths[i], "features.tsv.gz"),
+      cells = here(input_data_path, paths[i], "barcodes.tsv.gz")
+    )
+    colnames(runs_list[[runs[i]]]) <- paste(runs[i], colnames(runs_list[[runs[i]]]), sep = "_")
+    runs_list[[runs[i]]] <- Seurat::CreateSeuratObject(counts = as.matrix(runs_list[[runs[i]]]), min.cells = 1, min.features = 1)
+    runs_list[[runs[i]]][["percent.mt"]] <- Seurat::PercentageFeatureSet(runs_list[[runs[i]]], pattern = "^MT-")
+    runs_list[[runs[i]]][["percent.rps"]] <- Seurat::PercentageFeatureSet(runs_list[[runs[i]]], pattern = "^RPS")
+    runs_list[[runs[i]]]@meta.data$orig.ident <- runs[i]
+  }
+  
+  return(runs_list)
+  
+}
+
+
+#' Plot quality metrics as pdf
+#'
+#' @param figures_path character vector of output figures path
+#' @return NULL
+do_qc <- function(figures_path = figures_path){
+  
+  filename <- here::here(figures_path, "01_quality-check.pdf")
+  pdf(filename, 29.7 / 2.54, 21 / 2.54, useDingbats = FALSE)
+  for (i in seq_along(runs_list)) {
+    plot(0, type = "n", axes = FALSE, xlab = "", ylab = "", xlim = c(0, 1))
+    number_cells <- ncol(runs_list[[runs[i]]]@assays$RNA@data)
+    mtext(paste0(runs[i], " (", number_cells, ")"), side = 3, line = -2, cex = 3, at = -0.04, font = 3, adj = 0)
+    print(Seurat::VlnPlot(object = runs_list[[runs[i]]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt", "percent.rps")), main = runs[i])
+    print(Seurat::FeatureScatter(runs_list[[runs[i]]], feature1 = "nCount_RNA", feature2 = "percent.mt"))
+    print(Seurat::FeatureScatter(runs_list[[runs[i]]], feature1 = "nFeature_RNA", feature2 = "percent.mt"))
+    print(Seurat::FeatureScatter(runs_list[[runs[i]]], feature1 = "percent.rps", feature2 = "percent.mt"))
+    hist(runs_list[[runs[i]]]$nCount_RNA, col = "grey80", breaks = 100)
+    hist(runs_list[[runs[i]]]$nFeature_RNA, col = "grey80", breaks = 100)
+    hist(na.omit(runs_list[[runs[i]]]$percent.mt), col = "grey80", breaks = 100)
+    hist(na.omit(runs_list[[runs[i]]]$percent.rps), col = "grey80", breaks = 100)
+  }
+  dev.off()
+  
+}
+
+#' Filter seurat objects and plot filtered quality metrics as pdf
+#'
+#' @param runs_list list of seurat objects to filter and plot
+#' @param figures_path character vector of output figures path
+#' @return list of seurat objects
+do_filtering_and_qc <- function(runs_list = runs_list, figures_path = figures_path){
+
+  filename <- here(figures_path, "01_filtered-quality-check.pdf")
+  pdf(filename, 29.7 / 2.54, 21 / 2.54, useDingbats = FALSE)
+  
+  for (i in seq_along(runs_list)) {
+    filtering_cutoff_list[[runs[i]]] <- c(
+      "min_nCount_RNA" = quantile(runs_list[[runs[i]]]$nCount_RNA, 0.25),
+      "max_nCount_RNA" = median(runs_list[[runs[i]]]$nCount_RNA) + sd(runs_list[[runs[i]]]$nCount_RNA) * 3,
+      "max_percent.mt" = median(runs_list[[runs[i]]]$percent.mt) + sd(runs_list[[runs[i]]]$percent.mt) * 3,
+      "max_percent.rps" = quantile(runs_list[[runs[i]]]$percent.rps, 0.30),
+      "min_nFeature_RNA" = quantile(runs_list[[runs[i]]]$nFeature_RNA, 0.25)
+    )
+    
+    # filtering out dead cells
+    runs_list[[runs[i]]] <- subset(runs_list[[runs[i]]],
+                                   subset = nCount_RNA < filtering_cutoff_list[[i]]["min_nCount_RNA"] & percent.mt > filtering_cutoff_list[[i]]["max_percent.mt"] |
+                                     nFeature_RNA < filtering_cutoff_list[[i]]["min_nFeature_RNA"] & percent.mt > filtering_cutoff_list[[i]]["max_percent.mt"] |
+                                     percent.mt > filtering_cutoff_list[[i]]["max_percent.mt"] & percent.rps < filtering_cutoff_list[[i]]["max_percent.rps"], invert = TRUE
+    )
+    
+    # filtering out doublets
+    runs_list[[runs[i]]] <- subset(runs_list[[runs[i]]],
+                                   subset = nCount_RNA < filtering_cutoff_list[[i]]["max_nCount_RNA"]
+    )
+    
+    
+    plot(0, type = "n", axes = FALSE, xlab = "", ylab = "", xlim = c(0, 1))
+    number_cells <- ncol(runs_list[[runs[i]]]@assays$RNA@data)
+    mtext(paste0(runs[i], " (", number_cells, ")"), side = 3, line = -2, cex = 3, at = -0.04, font = 3, adj = 0)
+    print(VlnPlot(object = runs_list[[runs[i]]], features = c("nFeature_RNA", "nCount_RNA", "percent.mt")), main = runs[i])
+    print(FeatureScatter(runs_list[[runs[i]]], feature1 = "nCount_RNA", feature2 = "percent.mt"))
+    print(FeatureScatter(runs_list[[runs[i]]], feature1 = "nFeature_RNA", feature2 = "percent.mt"))
+    print(FeatureScatter(runs_list[[runs[i]]], feature1 = "percent.rps", feature2 = "percent.mt"))
+    hist(runs_list[[runs[i]]]$nCount_RNA, col = "grey80", breaks = 100)
+    hist(runs_list[[runs[i]]]$nFeature_RNA, col = "grey80", breaks = 100)
+    hist(na.omit(runs_list[[runs[i]]]$percent.mt), col = "grey80", breaks = 100)
+    hist(na.omit(runs_list[[runs[i]]]$percent.rps), col = "grey80", breaks = 100)
+  }
+  
+  dev.off()
+  
+  filename <- here(figures_path, "01_top-10-variable-features.pdf")
+  pdf(filename, 29.7 / 2.54, 21 / 2.54, useDingbats = FALSE)
+  for (i in seq_along(runs_list)) {
+    runs_list[[runs[i]]] <- NormalizeData(runs_list[[runs[i]]]) %>%
+      FindVariableFeatures(do.plot = T, verbose = F) %>%
+      ScaleData()
+    plot1 <- VariableFeaturePlot(runs_list[[runs[i]]])
+    plot2 <- LabelPoints(plot = plot1, points = head(VariableFeatures(runs_list[[runs[i]]]), 10), repel = TRUE)
+    print(plot1 + plot2 + ggtitle(runs[i]))
+  }
+  dev.off()
+  
+  return(runs_list)
+    
+}
+
+#' Save seurat objects in list
+#'
+#' @param runs_list list of seurat objects to filter and plot
+#' @param output_data_path character vector of output data path
+#' @return list of seurat objects
+save_seurat_objects <- function(output_data_path = output_data_path, runs_list = runs_list){
+ 
+  for (i in seq_along(runs_list)) {
+    
+    seurat_obj <- runs_list[[i]]
+    filename <- here(output_data_path, paste0(names(runs_list)[i], "_seurat-obj.RData"))
+    save(seurat_obj, file = filename)
+    
+  }
+  
+}
