ebbe60a6c4aec5ac171a14ac1c7f1a68793640cc
mschmidt000 <schmidt@izbi.uni-leipzig.de>
2022-06-10 14:13:44 +0200

    Functionalized code

--- src/02_reduce-dimension-and-cluster.r
+++ src/02_reduce-dimension-and-cluster.r
@@ -1,47 +1,11 @@
 ### reduce dimension and find clusters
 ### 19.05.22
 
-library(clustree)
+# reduce dimension and plot -----------------------------------------------
 
-n_dims_use <- 30
-
-filename <- here(figures_path, "02_dimension-reduction-and-clustering.pdf")
-pdf(filename, 29.7 / 2.54, 21 / 2.54, useDingbats = FALSE)
-for (i in 1:length(runs_list)) {
-  perplexity <- sqrt(ncol(runs_list[[runs[i]]]@assays$RNA@counts))
-  runs_list[[runs[i]]] <- runs_list[[runs[i]]] %>%
-    RunPCA(features = VariableFeatures(runs_list[[runs[i]]])) %>%
-    FindNeighbors(dims = 1:n_dims_use) %>%
-    FindNeighbors(dims = 1:n_dims_use) %>%
-    FindClusters(resolution = c(0.2, 0.4, 0.5, 0.6, 0.8, 1)) %>%
-    RunTSNE(dims = 1:n_dims_use, perplexity = perplexity) %>%
-    RunUMAP(dims = 1:n_dims_use) %>%
-    CellCycleScoring(
-      s.features = cc.genes$s.genes,
-      g2m.features = cc.genes$g2m.genes,
-      set.ident = FALSE
-    )
-
-  plot(0, type = "n", axes = FALSE, xlab = "", ylab = "", xlim = c(0, 1))
-  mtext(runs[i], side = 3, line = -2, cex = 3, at = -0.04, font = 3, adj = 0)
-
-  plot(VizDimLoadings(runs_list[[runs[i]]], dims = 1:2, reduction = "pca"))
-  plot(DimPlot(runs_list[[runs[i]]], reduction = "pca"))
-  plot(ElbowPlot(runs_list[[runs[i]]]))
-  plot(FeaturePlot(runs_list[[runs[i]]],
-    reduction = "tsne",
-    features = c("percent.mt", "percent.rps", "nCount_RNA", "nFeature_RNA")
-  ))
-  plot(DimPlot(runs_list[[runs[i]]], reduction = "tsne", label = TRUE, group.by = c("seurat_clusters", "Phase")))
-  plot(DimPlot(runs_list[[runs[i]]], reduction = "umap", label = TRUE, group.by = c("seurat_clusters", "Phase")))
-  clustree(x = runs_list[[runs[i]]]@meta.data, prefix = "RNA_snn_res.") + ggtitle("Clustering Tree visualizing Clusterings at Different Clustering-Resolutions")
-}
-dev.off()
+map(run_list, ~reduce_dimension_and_cluster(seurat_object = .x, figures_path = figures_path))
 
 
-for (i in seq_along(runs_list)) {
-  seurat_obj <- runs_list[[runs[i]]]
-  filename <- here(output_data_path, paste0(runs[i], "_seurat-obj.RData"))
-  save(seurat_obj, file = filename)
-}
-rm(seurat_obj)
+# save individual seurat objects ------------------------------------------
+
+map(runs_list, ~save_seurat_objects(seurat_object = .x, output_data_path = output_data_path))
--- src/seurat-functions.r
+++ src/seurat-functions.r
@@ -1,22 +1,25 @@
-#' Create seurat object, set cell names with run-prefix and calculate mitochondrial and ribosomal expression percentage
+#' Create seurat object, set cell name with run-prefix and calculate mitochondrial and ribosomal expression percentage
 #'
 #' @param data_set_name sequence of characters representing the name of the data set
 #' @param input_data_folder sequence of characters representing the individual input data folder name
 #' @param input_data_path character vector of general input data path
 #' @return seurat object
 create_seurat_object <- function(data_set_name = data_set_name, input_data_folder = input_data_folder, input_data_path = input_data_path){
+  
     pacman::p_load(Seurat, tidyverse, here)
+  
     seurat_object <- ReadMtx(
       mtx = here(input_data_path, input_data_folder, "matrix.mtx.gz"), features = here(input_data_path, input_data_folder, "features.tsv.gz"),
       cells = here(input_data_path, input_data_folder, "barcodes.tsv.gz")
     )
     colnames(seurat_object) <- paste(data_set_name, colnames(seurat_object), sep = "_")
-    seurat_object <- CreateSeuratObject(counts = as.matrix(seurat_object), min.cells = 1, min.features = 1)
+    
+    seurat_object <- CreateSeuratObject(counts = as.matrix(seurat_object), min.cells = 1, min.features = 3)
     seurat_object[["percent.mt"]] <- PercentageFeatureSet(seurat_object, pattern = "^MT-")
     seurat_object[["percent.rps"]] <- PercentageFeatureSet(seurat_object, pattern = "^RPS")
     seurat_object@meta.data$orig.ident <- data_set_name
-
-  seurat_object
+    seurat_object <- find_doublets(seurat_object)
+    seurat_object
   
 }
 
@@ -30,26 +33,38 @@ do_qc <- function(seurat_object = seurat_object, figures_path = figures_path){
   
   name_run <- unique(seurat_object$orig.ident)
   pacman::p_load(Seurat, tidyverse, here)
+  
   filename <- here(figures_path, paste("01", name_run, "quality-check.pdf", sep = "_"))
   pdf(filename, 29.7 / 2.54, 21 / 2.54, useDingbats = FALSE)
-    plot(0, type = "n", axes = FALSE, xlab = "", ylab = "", xlim = c(0, 1))
-    number_cells <- ncol(seurat_object@assays$RNA@data)
-    mtext(paste0(name_run, " (", number_cells, ")"), side = 3, line = -2, cex = 3, at = -0.04, font = 3, adj = 0)
-    print(VlnPlot(object = seurat_object, features = c("nFeature_RNA", "nCount_RNA", "percent.mt", "percent.rps")), main = name_run)
-    print(FeatureScatter(seurat_object, feature1 = "nCount_RNA", feature2 = "percent.mt"))
-    print(FeatureScatter(seurat_object, feature1 = "nFeature_RNA", feature2 = "percent.mt"))
-    print(FeatureScatter(seurat_object, feature1 = "percent.rps", feature2 = "percent.mt"))
-    hist(seurat_object$nCount_RNA, col = "grey80", breaks = 100)
-    hist(seurat_object$nFeature_RNA, col = "grey80", breaks = 100)
-    hist(na.omit(seurat_object$percent.mt), col = "grey80", breaks = 100)
-    hist(na.omit(seurat_object$percent.rps), col = "grey80", breaks = 100)
- dev.off()
+  
+  plot(0, type = "n", axes = FALSE, xlab = "", ylab = "", xlim = c(0, 1))
+  number_cells <- ncol(seurat_object@assays$RNA@data)
+  mtext(paste0(name_run, " (", number_cells, ")"), side = 3, line = -2, cex = 3, at = -0.04, font = 3, adj = 0)
+  print(VlnPlot(object = seurat_object, features = c("nFeature_RNA", "nCount_RNA", "percent.mt", "percent.rps")), main = name_run)
+  print(FeatureScatter(seurat_object, feature1 = "nCount_RNA", feature2 = "nFeature_RNA" , group.by = colnames(seurat_object@meta.data)[grep("DF.class",colnames(seurat_object@meta.data))]))
+  print(FeatureScatter(seurat_object, feature1 = "nCount_RNA", feature2 = "percent.mt"))
+  print(FeatureScatter(seurat_object, feature1 = "nFeature_RNA", feature2 = "percent.mt"))
+  print(FeatureScatter(seurat_object, feature1 = "percent.rps", feature2 = "percent.mt"))
+  hist(seurat_object$nCount_RNA, col = "grey80", breaks = 100)
+  hist(seurat_object$nFeature_RNA, col = "grey80", breaks = 100)
+  hist(na.omit(seurat_object$percent.mt), col = "grey80", breaks = 100)
+  hist(na.omit(seurat_object$percent.rps), col = "grey80", breaks = 100)
+  
+  plot(FeaturePlot(seurat_object,
+                   reduction = "umap",
+                   features = c("percent.mt", "percent.rps", "nCount_RNA", "nFeature_RNA")
+  ))
+  plot(DimPlot(seurat_object, reduction = "umap", label = TRUE, group.by = c("seurat_clusters")))
+  clustree(x = seurat_object@meta.data, prefix = "RNA_snn_res.") 
+  plot(DimPlot(seurat_object, reduction = "umap", label = TRUE, group.by = colnames(seurat_object@meta.data)[grep("DF.class",colnames(seurat_object@meta.data))]))
+  
+  dev.off()
  
- seurat_object
+  seurat_object
   
 }
 
-#' Filter seurat objects and plot filtered quality metrics as pdf
+#' Filter seurat object and plot filtered quality metrics as pdf
 #'
 #' @param seurat_object seurat object
 #' @param figures_path character vector of output figures path
@@ -61,12 +76,12 @@ do_filtering_and_qc <- function(seurat_object = seurat_object, figures_path = fi
   filename <- here(figures_path, paste("01", name_run, "filtered-quality-check.pdf", sep = "_"))
   pdf(filename, 29.7 / 2.54, 21 / 2.54, useDingbats = FALSE)
   
+  # seurat_object <- remove_ambient_expression(seurat_object, name_run, input_data_folder, input_data_path)
+  
   filtering_cutoff_list <- list(
-    "min_nCount_RNA" = quantile(seurat_object$nCount_RNA, 0.25),
-    "max_nCount_RNA" = median(seurat_object$nCount_RNA) + sd(seurat_object$nCount_RNA) * 3,
-    "max_percent.mt" = median(seurat_object$percent.mt) + sd(seurat_object$percent.mt) * 3,
-    "max_percent.rps" = quantile(seurat_object$percent.rps, 0.30),
-    "min_nFeature_RNA" = quantile(seurat_object$nFeature_RNA, 0.25)
+    "min_nCount_RNA" = quantile(seurat_object$nCount_RNA, 0.1),
+    "min_nFeature_RNA" = quantile(seurat_object$nFeature_RNA, 0.1),
+    "max_percent.mt" = median(seurat_object$percent.mt) + sd(seurat_object$percent.mt) * 3
   )
     
   # filtering out dead cells
@@ -77,15 +92,13 @@ do_filtering_and_qc <- function(seurat_object = seurat_object, figures_path = fi
   )
   
   # filtering out doublets
-  seurat_object <- subset(seurat_object,
-                                 subset = nCount_RNA < filtering_cutoff_list["max_nCount_RNA"]
-  )
-  
-  
+  seurat_object <- subset(seurat_object, subset = colnames(seurat_object@meta.data)[grep("DF.class",colnames(seurat_object@meta.data))] == "Singlet")
+
   plot(0, type = "n", axes = FALSE, xlab = "", ylab = "", xlim = c(0, 1))
   number_cells <- ncol(seurat_object@assays$RNA@data)
   mtext(paste0(name_run, " (", number_cells, ")"), side = 3, line = -2, cex = 3, at = -0.04, font = 3, adj = 0)
   print(VlnPlot(object = seurat_object, features = c("nFeature_RNA", "nCount_RNA", "percent.mt")), main = name_run)
+  print(FeatureScatter(seurat_object, feature1 = "nCount_RNA", feature2 = "nFeature_RNA", group.by = colnames(seurat_object@meta.data)[grep("DF.class",colnames(seurat_object@meta.data))]))
   print(FeatureScatter(seurat_object, feature1 = "nCount_RNA", feature2 = "percent.mt"))
   print(FeatureScatter(seurat_object, feature1 = "nFeature_RNA", feature2 = "percent.mt"))
   print(FeatureScatter(seurat_object, feature1 = "percent.rps", feature2 = "percent.mt"))
@@ -93,7 +106,15 @@ do_filtering_and_qc <- function(seurat_object = seurat_object, figures_path = fi
   hist(seurat_object$nFeature_RNA, col = "grey80", breaks = 100)
   hist(na.omit(seurat_object$percent.mt), col = "grey80", breaks = 100)
   hist(na.omit(seurat_object$percent.rps), col = "grey80", breaks = 100)
-
+  
+  plot(FeaturePlot(seurat_object,
+                   reduction = "umap",
+                   features = c("percent.mt", "percent.rps", "nCount_RNA", "nFeature_RNA")
+  ))
+  plot(DimPlot(seurat_object, reduction = "umap", label = TRUE, group.by = c("seurat_clusters")))
+  clustree(x = seurat_object@meta.data, prefix = "RNA_snn_res.") 
+  plot(DimPlot(seurat_object, reduction = "umap", label = TRUE, group.by = colnames(seurat_object@meta.data)[grep("DF.class",colnames(seurat_object@meta.data))]))
+  
   seurat_object <- NormalizeData(seurat_object) %>%
     FindVariableFeatures(do.plot = T, verbose = F) %>%
     ScaleData()
@@ -107,7 +128,7 @@ do_filtering_and_qc <- function(seurat_object = seurat_object, figures_path = fi
     
 }
 
-#' Save seurat objects in list
+#' Save seurat object in list
 #'
 #' @param seurat_object seurat object
 #' @param output_data_path character vector of output data path
@@ -119,3 +140,128 @@ save_seurat_objects <- function(seurat_object = seurat_object, output_data_path 
     save(seurat_object, file = filename)
 
 }
+
+#' Finds doublets
+#'
+#' @param seurat_object seurat object
+#' @return filtered seurat object
+find_doublets <- function(seurat_object = seurat_object){
+  
+  multiplet_rate_table <- tibble(rate = c(0.4, 0.8, 1.6, 2.3, 3.1, 3.9, 4.6, 5.4, 6.1, 6.9, 7.6),
+                           cell_recovered_1 = c(500, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000),
+                           cell_recovered_2 = c(1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 1000000))
+  
+  multiplet_rate_table <- multiplet_rate_table %>%
+                      mutate(is_between = ncol(seurat_object) >= cell_recovered_1 & ncol(seurat_object) <= cell_recovered_2)
+  
+  multiplet_rate <- multiplet_rate_table$rate[which(multiplet_rate_table$is_between)] * 0.01
+  
+  pacman::p_load(Seurat, tidyverse, here, DoubletFinder)
+  
+  n_dims_use <- 30
+  
+  perplexity <- sqrt(ncol(seurat_object@assays$RNA@counts))
+  seurat_object <- seurat_object %>%
+  RunPCA(features = VariableFeatures(seurat_object)) %>%
+  FindNeighbors(dims = 1:n_dims_use) %>%
+  FindClusters(resolution = 1.2)
+  
+  sweep.res.list <- paramSweep_v3(seurat_object, PCs = 1:10, sct = FALSE)
+  sweep.stat <- summarizeSweep(sweep.res.list, GT = FALSE)
+  bcmvn <- find.pK(sweep.stat)
+  pK <- bcmvn %>%
+    filter(BCmetric == max(BCmetric)) %>%
+    select(pK)
+  pK <- as.numeric(as.character(pK[[1]]))
+  
+  annotations <- seurat_object$seurat_clusters
+  homotypic.prop <- modelHomotypic(annotations)
+  nExp_poi <- round(multiplet_rate*ncol(seurat_object))
+  nExp_poi.adj <- round(nExp_poi*(1-homotypic.prop))
+  
+  seurat_object <- doubletFinder_v3(
+    seurat_object,
+    PCs = 1:n_dims_use,
+    pN = 0.25,
+    pK = pK,
+    nExp = nExp_poi.adj,
+    reuse.pANN = FALSE,
+    sct = FALSE
+  ) 
+  
+  seurat_object
+  
+}
+
+
+#' Remove free mRNA contamination of cell 
+#'
+#' @param seurat_object seurat object
+#' @param data_set_name sequence of characters representing the name of the data set
+#' @param input_data_folder sequence of characters representing the individual input data folder name
+#' @param input_data_path character vector of general input data path
+#' @return filtered data matrix
+remove_ambient_expression <- function(seurat_object = seurat_object, data_set_name = data_set_name, input_data_folder = input_data_folder, input_data_path = input_data_path){
+  
+  pacman::p_load(Seurat, here, SoupX)
+  
+  toc = GetAssayData(seurat_object, slot = "counts")
+  tod = Read10X(here(input_data_folder, input_data_path, "raw_gene_bc_matrices"))
+  sc = SoupChannel(tod, toc)
+  sc = setClusters(sc,seurat_object$seurat_clusters)
+  sc = autoEstCont(sc)
+  out = adjustCounts(sc)
+
+}
+
+#' Dimension reduction and clustering
+#'
+#' @param seurat_object seurat object
+#' @param figures_path character vector of output figures path
+#' @return seurat object
+reduce_dimension_and_cluster <- function(seurat_object = seurat_object, figures_path = figures_path){
+  
+  pacman::p_load(Seurat, tidyverse, here, clustree)
+  
+  n_dims_use <- 30
+  
+  perplexity <- sqrt(ncol(seurat_object))
+  seurat_object <- seurat_object %>%
+    RunPCA(features = VariableFeatures(seurat_object)) %>%
+    FindNeighbors(dims = 1:n_dims_use) %>%
+    FindClusters(resolution = c(0.2, 0.4, 0.5, 0.6, 0.8, 1)) %>%
+    RunTSNE(dims = 1:n_dims_use, perplexity = perplexity) %>%
+    RunUMAP(dims = 1:n_dims_use) %>%
+    CellCycleScoring(
+      s.features = cc.genes$s.genes,
+      g2m.features = cc.genes$g2m.genes,
+      set.ident = FALSE
+    )
+  
+  name_run <- unique(seurat_object$orig.ident)
+
+  filename <- here(figures_path, paste("02", name_run, "dimension-reduction-and-clustering.pdf", sep = "_"))
+  pdf(filename, 29.7 / 2.54, 21 / 2.54, useDingbats = FALSE)
+  
+  plot(0, type = "n", axes = FALSE, xlab = "", ylab = "", xlim = c(0, 1))
+  mtext(name_run, side = 3, line = -2, cex = 3, at = -0.04, font = 3, adj = 0)
+  
+  plot(VizDimLoadings(seurat_object, dims = 1:2, reduction = "pca"))
+  plot(DimPlot(seurat_object, reduction = "pca"))
+  plot(ElbowPlot(seurat_object))
+  plot(FeaturePlot(seurat_object,
+                   reduction = "umap",
+                   features = c("percent.mt", "percent.rps", "nCount_RNA", "nFeature_RNA")
+  ))
+  plot(DimPlot(seurat_object, reduction = "tsne", label = TRUE, group.by = c("seurat_clusters", "Phase")))
+  plot(DimPlot(seurat_object, reduction = "umap", label = TRUE, group.by = c("seurat_clusters", "Phase")))
+  clustree(x = seurat_object@meta.data, prefix = "RNA_snn_res.") 
+  plot(DimPlot(seurat_object, reduction = "umap", label = TRUE, group.by = colnames(seurat_object@meta.data)[grep("DF.class",colnames(seurat_object@meta.data))]))
+
+
+  dev.off()
+  
+  seurat_object
+  
+}
+
