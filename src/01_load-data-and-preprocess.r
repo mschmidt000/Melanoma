5417f84ad13feac80897f9366c7f1c9502199862
mschmidt000 <schmidt@izbi.uni-leipzig.de>
2022-06-22 13:30:04 +0200

    Add LE-599-SH_GEX

--- src/01_load-data-and-preprocess.r
+++ src/01_load-data-and-preprocess.r
@@ -13,7 +13,7 @@ source(here("src","seurat-functions.r"))
 runs <- c(
   "s1_LE479_NM_T", "LE489KG_Rep", "LE493BR_Rep", "LE497NA_Rep", "LE497ST_Rep", "LE-501-DM_Rep", "LE-517-LD",
   "LE-511-MW", "LE_569_HH", "LE_577_WR", "LE_579_DE", "LE-583-KM", "LE-585-HM", "LE-597-EG_GEX", "LE-595-SV_GEX",
-  "LE-593-KP_RNAseq", "LE-589-BE_RNAseq"
+  "LE-593-KP_RNAseq", "LE-589-BE_RNAseq", "LE-599-SH_GEX"
 )
 input_data_path <- file.path("data")
 output_data_path <- file.path("output")
