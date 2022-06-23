6e0e08948dfc4118bedb1e06b4e785d4d76f62a0
mschmidt000 <schmidt@izbi.uni-leipzig.de>
2022-06-23 09:38:09 +0200

    Bugfix

--- src/02_reduce-dimension-and-cluster.r
+++ src/02_reduce-dimension-and-cluster.r
@@ -5,9 +5,9 @@
 
 for(i in seq_along(runs)){
   seurat_object <- load_seurat_object(data_set_name = runs[i],  output_data_path = output_data_path) %>%
-                    reduce_dimension_and_cluster(figures_path = figures_path)
+  reduce_dimension_and_cluster(figures_path = figures_path)
   
-  save_seurat_objects(seurat_object = seurat_object, output_data_path = output_data_path)
+  save_seurat_object(seurat_object = seurat_object, output_data_path = output_data_path)
   rm(seurat_object)
 }
 
